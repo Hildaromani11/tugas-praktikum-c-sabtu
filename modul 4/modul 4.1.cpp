e2eccf4d3f33da1565c75f0310fd4b21a397bad4
Hildaromani11 <hildaaromanii@gmail.com>
2023-10-28 18:44:31 +0700

    tugas praktikum sabtu

---
+++ modul 4/modul 4.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+
+int main()
+{
+    int n;
+    printf("\nInput n = ");
+    scanf("%d", &n);
+    
+    if(n > 50){
+	    n = n + 10;
+    }else{
+    	n = n - 25;
+    }  
+    printf("\n%d", n);
+    return 0;
+}
+
